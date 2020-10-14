0172c4053b38319d61937c91d7a3491ef738ffc0
Alexis Lemaire <a.lemaire@klimapartner.de>
2020-10-14 14:58:37 +0200

    Fixed indentation in style tag

--- lambdas/serve/serve.js
+++ lambdas/serve/serve.js
@@ -9,12 +9,12 @@ module.exports.handler = async (event) => {
   <link rel="icon" type="image/png" href="${host}/imgs/icons/favicon.png">
   <script type="module" src="${host}/modules/@kaskadi/kaskadi-apps/kaskadi-app.js"></script>
   <style>
-  html, body {
-    margin: 0;
-    padding: 0;
-    font-family: sans-serif;
-    font-size: 16px;
-  }
+    html, body {
+      margin: 0;
+      padding: 0;
+      font-family: sans-serif;
+      font-size: 16px;
+    }
   </style>
 </head>
 <body>
